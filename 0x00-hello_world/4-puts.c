693dcf951a88d9d1317ecc9cd91d5605d1927e96
“PiRwot” <agojos@gmail.com>
2022-10-22 11:12:00 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-	int main(void)
-		{
-			puts("Programming is like building a multilingual puzzle\n");
-			return (0);
-		}
+
+#/**
+   *main - this program prints Programming is like
+   *return - this returns 0
+   */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
