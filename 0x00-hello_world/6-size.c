de646923373c0c62a7b63318f2a02ff3d5347c1a
“PiRwot” <agojos@gmail.com>
2022-10-22 13:42:46 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
 /**
-*main-Entry Point
+*main-Entry point
 *return: 0 if successful
 */
-int main(void) 
+int main(void)
 {
 char charType;
 int intType;
 long int longintType;
 long long int longlongintType;
 float floatType;
-
-printf("Size of char: %zu byte\n", sizeof(charType)); 
+printf("Size of char: %zu byte\n", sizeof(charType));
 printf("Size of int: %zu bytes\n", sizeof(intType));
 printf("Size of longint: %zu byte\n", sizeof(longintType));
 printf("Size of longlongint: %zu byte\n", sizeof(longlongintType));
