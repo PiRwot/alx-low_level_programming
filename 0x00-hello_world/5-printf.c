3ce736f3f7c314321cf9b6435fcfab28c650c58d
“PiRwot” <agojos@gmail.com>
2022-10-22 12:07:18 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
-* main-Entry poin
-*
-* return: 0 if successful
+*main-Entry point
+*return: 0 if successful
 */
-
-int main(void) 
-{ 
+int main(void)
+{
 printf("with proper grammar, but the outcome is a piece of art,\n"); 
-return (0); 
-} 
+return (0);
+}
