6140c05b904ca7718ae4cb2748e1a62d6417b048
PiRwot <agojos@gmail.com>
2023-02-21 10:41:57 +0000

    Combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,21 +6,21 @@
 
 int main(void)
 {
-	int tens;
-	int ones;
-	for (tens = '0'; tens <='9'; tens++) /*print tens place*/
-	{
-		for (ones = '0'; ones <= '9'; ones++) /*print ones place*/
-		{
-			putchar(tens);
-			putchar(ones);
-			if (!(tens == '9' && ones == '9')) /*skip comma at end*/
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+        int tens;
+        int ones;
+        for (tens = '0'; tens <='9'; tens++) /*print tens place*/
+        {
+                for (ones = '0'; ones <= '9'; ones++) /*print ones place*/
+                {
+                        putchar(tens);
+                        putchar(ones);
+                        if (!(tens == '9' && ones == '9')) /*skip comma at end*/
+                        {
+                                putchar(',');
+                                putchar(' ');
+                        }
+                }
+        }
+        putchar('\n');
+        return (0);
 }
