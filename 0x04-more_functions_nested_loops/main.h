3b70ffe1ba898c5e0089a353b0388df32aefa9d7
PiRwot <agojos@gmail.com>
2023-02-24 14:10:18 +0000

    Print_Numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,6 +6,7 @@ int main(void);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_number(int n);
 void print_numbers(void);
 void print_most_numbers(void);
 int mul(int a, int b);
