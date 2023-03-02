2a79a9a2da65cbaef503cbf0ad9e705d79b66b02
PiRwot <agojos@gmail.com>
2023-03-02 01:12:35 +0000

    Resset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - go into pointer address and change the value.
+ * @n: int type pointer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98; /* using the dereferencing operator, go to n and update n to be 98 instead of init val*/
+}
