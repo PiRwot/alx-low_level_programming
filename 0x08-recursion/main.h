47cb0dd744187e342e1ba374312d1c2719bb11a4
PiRwot <agojos@gmail.com>
2023-03-09 18:35:53 -0800

    WildCamp

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,6 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int substring_match(char *s1, char *s2, char *after_wldcd);
 
 #endif
