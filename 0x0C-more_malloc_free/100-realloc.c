79c52c3f7ab538a5741c848f5bc550ff49b11c55
PiRwot <agojos@gmail.com>
2023-03-18 06:12:04 -0700

    reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,43 +12,31 @@
  */
 
 
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *ptr1;
-	unsigned int x;
+	char *new_location, *new_ptr;
+	unsigned int i = 0;
 
-	if (!ptr)
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL) /* treat as normal malloc */
 	{
-		ptr1 = malloc(new_size);
-		if (!ptr1)
+		ptr = malloc(new_size);
+		if (ptr == NULL)
 			return (NULL);
-		return (ptr1);
+		return (ptr);
 	}
-
-	if (new_size == 0 && ptr != NULL)
+	if (new_size == 0 && ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
-	if (new_size == old_size)
-		return (ptr);
-
-	ptr1 = malloc(new_size);
-
-	if (!ptr1)
-		return (NULL);
-
-	if (new_size < old_size)
-		for (; x < new_size; x++)
-			((char *)ptr1)[x] = ((char *)ptr)[x];
-	else
-		for (; x < old_size; x++)
-			((char *)ptr1)[x] = ((char *)ptr)[x];
-
+	new_location = malloc(new_size);
+	new_ptr = ptr;
+	if (old_size > new_size)
+		old_size = new_size;
+	for (i = 0; i < old_size; i++)
+		new_location[i] = new_ptr[i];
 	free(ptr);
-
-	return (ptr1);
+	return (new_location);
 }
-
